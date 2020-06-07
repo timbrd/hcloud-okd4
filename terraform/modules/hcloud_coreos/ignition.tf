75f72acdd03bdb1e9ca47e61f6c51d54add433a4
Simon Lauger <simon@lauger.de>
2020-06-07 16:40:25 +0200

    add network config

--- terraform/modules/hcloud_coreos/ignition.tf
+++ terraform/modules/hcloud_coreos/ignition.tf
@@ -9,7 +9,6 @@ data "template_file" "ignition_config" {
   count = var.instance_count
 }
 
-
 #resource "local_file" "ignition_config" {
 #  content  = data.template_file.ignition_config[count.index].rendered
 #  filename = "${path.root}/${format("${var.name}%02d", count.index + 1)}.${var.dns_domain}.ign"
